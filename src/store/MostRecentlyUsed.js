e27e679dddb5bf9032dabb669e3d0879a51f1b17
Jeff Long <banterability@users.noreply.github.com>
2017-08-14 15:56:56 -0500

    Set up MRU list in localstorage

---
+++ src/store/MostRecentlyUsed.js
@@ -0,0 +1,41 @@
+const keyName = "slowzone:recent";
+
+class MostRecentlyUsed {
+  constructor(list = []) {
+    this.list = list;
+  }
+
+  push(key, item) {
+    const newItem = {...item, key};
+    const filteredList = this.list.filter(entry => entry.key !== key);
+
+    filteredList.push(newItem);
+    this.list = filteredList;
+    return this.list;
+  }
+
+  get() {
+    return this.list.slice().reverse().map(item => {
+      const {key, ...values} = item;
+      return values;
+    });
+  }
+
+  toJSON() {
+    return this.list;
+  }
+}
+
+export const getRecentStations = () => {
+  const data = window.localStorage.getItem(keyName) || "[]";
+  return new MostRecentlyUsed(JSON.parse(data)).get();
+};
+
+export const pushStation = (stationId, station) => {
+  const data = window.localStorage.getItem(keyName) || "[]";
+  const list = new MostRecentlyUsed(JSON.parse(data));
+
+  list.push(stationId, station);
+
+  window.localStorage.setItem(keyName, JSON.stringify(list));
+};
