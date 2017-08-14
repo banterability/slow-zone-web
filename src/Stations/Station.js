80d21c99aa7beff74cd05527e94e271601c31ea7
Jeff Long <banterability@users.noreply.github.com>
2017-08-14 15:56:29 -0500

    Update Station to fetch

--- src/Stations/Station.js
+++ src/Stations/Station.js
@@ -1,19 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
 
+import {StationRequest} from "../requests";
+
+import ArrivalList from "./ArrivalList";
 import Line from "./Line";
 
 class Station extends React.Component {
-  render() {
-    const {name, lines} = this.props;
+  state = {
+    loading: true,
+    station: {}
+  };
 
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        stationId: PropTypes.string.isRequired
+      }).isRequired
+    }).isRequired
+  };
+
+  fetchStation = () =>
+    fetch(
+      new StationRequest({
+        url: `/${this.props.match.params.stationId}`
+      })
+    )
+      .then(res => res.json())
+      .then(json => json.data)
+      .then(station => {
+        this.setState({station, loading: false});
+      });
+
+  componentDidMount() {
+    this.fetchStation();
+  }
+
+  _renderStation = () => {
+    const {station: {name, lines, arrivals}} = this.state;
     return (
       <div>
-        <p>{name}</p>
+        <h3>{name}</h3>
         <div>
           {lines.map((line, index) => <Line key={index} line={line} />)}
         </div>
+        <ArrivalList arrivals={arrivals} />
       </div>
     );
+  };
+
+  render() {
+    return this.state.loading ? <p>Loading</p> : this._renderStation();
   }
 }
 
