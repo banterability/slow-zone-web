d5a26f2b35d04d8fa9d2a5654f1465c561e62300
Jeff Long <banterability@users.noreply.github.com>
2017-11-08 09:03:56 -0600

    Add recentstation type; export types

---
+++ src/types/RecentStation.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+import type {Line} from "./Line";
+
+export type RecentStation = {
+  key: number,
+  lines: Array<Line>,
+  pathname: string,
+  title: string
+};
---
+++ src/types/index.js
@@ -0,0 +1,6 @@
+export * from "./Arrival";
+export * from "./Line";
+export * from "./Location";
+export * from "./RecentStation";
+export * from "./Station";
+export * from "./Stop";
