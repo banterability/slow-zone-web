21ebf4ea82a7fe5287b26145a652981d5dcb3164
Jeff Long <banterability@users.noreply.github.com>
2017-12-05 15:29:37 -0600

    Add flow typing to DirectionFilter

--- src/Stations/Filters/DirectionFilter.js
+++ src/Stations/Filters/DirectionFilter.js
@@ -1,10 +1,25 @@
+//@flow
 import React from "react";
 
 import Filter from "./Filter";
 
-class DirectionFilter extends React.Component {
+type Props = {
+  description: string,
+  directions: Array<string>,
+  onFilter: Function,
+  onReset: Function
+};
+
+class DirectionFilter extends React.Component<Props> {
   render() {
-    return <Filter description="Direction" />;
+    return (
+      <Filter
+        description="Direction"
+        options={this.props.directions}
+        onFilter={this.props.onFilter}
+        onReset={this.props.onReset}
+      />
+    );
   }
 }
 
