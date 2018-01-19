b225045608a65b7d5258f8ff05368ba59c22ff5e
Jeff Long <banterability@users.noreply.github.com>
2018-01-19 15:35:30 -0600

    Add LoadingBar component

---
+++ src/Components/LoadingBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LoadingBar.css";
+
+class LoadingBar extends React.Component {
+  render() {
+    return (
+      <div className="loading-bar">
+        <div className="loading-bar--inner" />
+      </div>
+    );
+  }
+}
+
+export default LoadingBar;
