10fa18894f7173413a26464e7f44700dbf834753
Jeff Long <banterability@users.noreply.github.com>
2018-01-29 21:24:34 -0600

    Add react-router flow types; update stations

--- src/Stations/Stations.js
+++ src/Stations/Stations.js
@@ -1,10 +1,12 @@
+// @flow
 import React from "react";
 import {Route} from "react-router-dom";
+import type {Match as MatchType} from "react-router-dom";
 
 import Station from "./Station";
 import StationListFetch from "./StationListFetch";
 
-class Stations extends React.Component {
+class Stations extends React.Component<{match: MatchType}> {
   render() {
     const {match} = this.props;
 
