1b544f91768e061bbb1b59a4a1130bb40d7c4e76
Jeff Long <banterability@users.noreply.github.com>
2018-04-16 17:11:23 -0500

    Remove state from station

--- src/Stations/Station.js
+++ src/Stations/Station.js
@@ -1,51 +1,19 @@
 // @flow
-
-import {StationRequest} from "../requests";
-import {pushStation} from "../store/RecentStations";
 import React, {Fragment} from "react";
 
 import ArrivalList from "./ArrivalList/ArrivalList";
 import Lines from "./Lines";
 
-import type {Match as MatchType} from "react-router";
 import type {Station as StationType, Arrival as ArrivalType} from "../types";
 
-type Props = {match: MatchType};
-type State = {
+type Props = {
   arrivals: Array<ArrivalType>,
-  loading: boolean,
   station: StationType
 };
 
-class Station extends React.Component<Props, State> {
-  state = {
-    arrivals: [],
-    loading: true,
-    station: {}
-  };
-
-  fetchStation = () =>
-    fetch(
-      new StationRequest({
-        url: `/${this.props.match.params.stationId}`
-      })
-    )
-      .then(res => res.json())
-      .then(({station, arrivals}) => {
-        pushStation(station.id, {
-          lines: station.lines,
-          title: station.name,
-          pathname: window.location.pathname
-        });
-        this.setState({station, arrivals, loading: false});
-      });
-
-  componentDidMount() {
-    this.fetchStation();
-  }
-
-  _renderStation = () => {
-    const {station: {name, lines}, arrivals} = this.state;
+class Station extends React.Component<Props> {
+  render() {
+    const {station: {name, lines}, arrivals} = this.props;
     return (
       <Fragment>
         <h3>{name}</h3>
@@ -53,10 +21,6 @@ class Station extends React.Component<Props, State> {
         <ArrivalList arrivals={arrivals} />
       </Fragment>
     );
-  };
-
-  render() {
-    return this.state.loading ? <p>Loading</p> : this._renderStation();
   }
 }
 
