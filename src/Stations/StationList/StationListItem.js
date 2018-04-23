16b9c393c73f15ba44c2919eccc84b6b63f3b632
Jeff Long <banterability@users.noreply.github.com>
2018-04-23 15:53:40 -0500

    Vary station color by lines

--- src/Stations/StationList/StationListItem.js
+++ src/Stations/StationList/StationListItem.js
@@ -1,6 +1,7 @@
 // @flow
 import React from "react";
 import {Link} from "react-router-dom";
+import classNames from "classnames";
 
 import Distance from "../../Components/Distance";
 import Lines from "../Lines";
@@ -8,6 +9,7 @@ import Lines from "../Lines";
 import type {Line as LineType, Distance as DistanceType} from "../../types";
 
 import "./StationListItem.css";
+import "../Lines.css";
 
 type Props = {
   distance?: DistanceType,
@@ -28,17 +30,26 @@ class StationListItem extends React.Component<Props> {
   render() {
     const {name, lines, distance} = this.props;
 
+    const multipleLines = lines.length !== 1;
+
+    const classString = classNames("station-list__item", {
+      [`cta-${lines[0]}`]: !multipleLines && lines[0],
+      "cta-grey": multipleLines
+    });
+
     return (
-      <Link className="station-list__item" to={this.stationUrl()}>
+      <Link className={classString} to={this.stationUrl()}>
         <p className="station-list__item__station-name">{name}</p>
         {distance && (
           <div className="station-list__item__distance">
             <Distance feet={distance.feet} miles={distance.miles} />
           </div>
         )}
-        <div className="station-list__item__lines">
-          <Lines lines={lines} />
-        </div>
+        {multipleLines && (
+          <div className="station-list__item__lines">
+            <Lines lines={lines} />
+          </div>
+        )}
       </Link>
     );
   }
