4366816249204fbe3a6c6c19c9d7ea79b04cf5eb
Jeff Long <banterability@users.noreply.github.com>
2019-07-21 23:20:54 -0500

    Add sentry

--- src/config.js
+++ src/config.js
@@ -3,4 +3,6 @@
 if (!process.env.REACT_APP_API_BASE_URL) {
   throw new Error('Missing env: "REACT_APP_API_BASE_URL"');
 }
+
+export const sentryDsn: string = process.env.SENTRY_DSN;
 export const apiBaseUrl: string = process.env.REACT_APP_API_BASE_URL;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from '@sentry/browser';
 
+import {sentryDsn} from "../config";
 import App from "./App";
 import {unregister} from "./registerServiceWorker";
 
 import "./index.css";
+
+if (sentryDsn) {
+  Sentry.init({dsn: sentryDsn});
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 unregister();
