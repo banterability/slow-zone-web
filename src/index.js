739e4fa5fdd6c0e9e5497c0580255786ba907be6
Jeff Long <banterability@users.noreply.github.com>
2019-07-21 23:44:12 -0500

    Revision tracking via sentry

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as Sentry from '@sentry/browser';
 
-import {sentryDsn} from "../config";
+import {sentryDsn, revision} from "../config";
 import App from "./App";
 import {unregister} from "./registerServiceWorker";
 
 import "./index.css";
 
 if (sentryDsn) {
-  Sentry.init({dsn: sentryDsn});
+  Sentry.init({dsn: sentryDsn, release: revision});
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
