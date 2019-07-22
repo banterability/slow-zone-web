cf4cdbf4dd6a69e7095fb38865bb9ae39851a6e0
Jeff Long <banterability@users.noreply.github.com>
2019-07-22 15:03:28 -0500

    clean up config; alias env vars

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ if (!process.env.REACT_APP_API_BASE_URL) {
   throw new Error('Missing env: "REACT_APP_API_BASE_URL"');
 }
 
-export const revision: string = process.env.NOW_GITHUB_COMMIT_SHA;
-export const sentryDsn: string = process.env.SENTRY_DSN;
+export const revision: string = process.env.REACT_APP_REVISION;
+export const sentryDsn: string = process.env.REACT_APP_SENTRY_DSN;
 export const apiBaseUrl: string = process.env.REACT_APP_API_BASE_URL;
