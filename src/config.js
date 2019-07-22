414edb94e12a6b80265ff078bd186fe54293331b
Jeff Long <banterability@users.noreply.github.com>
2019-07-22 11:52:59 -0500

    Revert "remove sentry integration to debug"
    This reverts commit 4cbc97c08e2137e86ada64726420098d9d362e93.

--- src/config.js
+++ src/config.js
@@ -4,4 +4,6 @@ if (!process.env.REACT_APP_API_BASE_URL) {
   throw new Error('Missing env: "REACT_APP_API_BASE_URL"');
 }
 
+export const revision: string = process.env.NOW_GITHUB_COMMIT_SHA;
+export const sentryDsn: string = process.env.SENTRY_DSN;
 export const apiBaseUrl: string = process.env.REACT_APP_API_BASE_URL;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from '@sentry/browser';
 
+import {sentryDsn, revision} from "./config";
 import App from "./App";
 import {unregister} from "./registerServiceWorker";
 
 import "./index.css";
+
+if (sentryDsn) {
+  Sentry.init({dsn: sentryDsn, release: revision});
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 unregister();
