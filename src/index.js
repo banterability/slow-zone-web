7816ef3f33d1ce59aa9d3391ee3ae1bfd6ba72ca
Jeff Long <banterability@users.noreply.github.com>
2019-07-24 13:23:12 -0500

    sentry formatting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 
 import {sentryDsn, revision} from "./config";
 import App from "./App";
