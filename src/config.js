8a65894392ae72b2e5b7f840fc6ec09896fa94ee
Jeff Long <banterability@users.noreply.github.com>
2019-08-22 16:49:51 -0500

    update env variable handling

--- src/config.js
+++ src/config.js
@@ -1,13 +1,21 @@
 // @flow
 
-//. Required enviromnet variables
-if (!process.env.REACT_APP_API_BASE_URL) {
-  throw new Error('Missing env: "REACT_APP_API_BASE_URL"');
-}
-export const apiBaseUrl: string = process.env.REACT_APP_API_BASE_URL;
+const getEnvOptional = (variableName: string): string => {
+  return process.env[variableName] || "";
+};
 
-//. Optional enviromnet variables
-export const googleStaticMapsKey: string =
-  process.env.REACT_APP_GOOGLE_STATIC_MAPS_KEY || "";
-export const revision: string = process.env.REACT_APP_REVISION || "";
-export const sentryDsn: string = process.env.REACT_APP_SENTRY_DSN || "";
+const getEnvRequired = (variableName: string): string => {
+  if (!process.env[variableName]) {
+    throw new Error(
+      `[config] Missing required environment variable: ${variableName}`
+    );
+  }
+  return process.env[variableName];
+};
+
+export const apiBaseUrl = getEnvRequired("REACT_APP_API_BASE_URL");
+export const googleStaticMapsKey = getEnvOptional(
+  "REACT_APP_GOOGLE_STATIC_MAPS_KEY"
+);
+export const revision = getEnvOptional("REACT_APP_REVISION");
+export const sentryDsn = getEnvOptional("REACT_APP_SENTRY_DSN");
