c1295995d495a68cbbac823ebdb8559f2a17cd1b
Jeff Long <banterability@users.noreply.github.com>
2019-08-30 15:51:28 -0500

    distance -> func

--- src/Components/Distance.js
+++ src/Components/Distance.js
@@ -3,14 +3,11 @@ import React from "react";
 
 import type {Distance as DistanceType} from "../types";
 
-class Distance extends React.Component<DistanceType> {
-  render() {
-    const {feet, miles} = this.props;
-    const value =
-      miles > 0.25 ? `${miles.toFixed(1)} mi` : ` ${feet.toFixed(0)} feet`;
+const Distance = ({feet, miles}: DistanceType) => {
+  const value =
+    miles > 0.25 ? `${miles.toFixed(1)} mi` : ` ${feet.toFixed(0)} feet`;
 
-    return <span className="station-list__item__distance">{value}</span>;
-  }
-}
+  return <span className="station-list__item__distance">{value}</span>;
+};
 
 export default Distance;
