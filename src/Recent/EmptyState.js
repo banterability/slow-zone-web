745064694d9bcf87cb98b96e8d5eb4c838728cee
Jeff Long <banterability@users.noreply.github.com>
2019-09-30 10:55:52 -0500

    Redesign recent stations page

---
+++ src/Recent/EmptyState.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+
+const EmptyState = () => (
+  <div>
+    <p>You don't currently have any recent stations.</p>
+    <p>
+      Visit any station's page and it will automatically appear in this list.
+    </p>
+  </div>
+);
+
+export default EmptyState;
--- src/Recent/Recent.js
+++ src/Recent/Recent.js
@@ -1,25 +1,36 @@
 // @flow
 import React from "react";
+import {Link} from "react-router-dom";
 
+import EmptyState from "./EmptyState";
 import StationListItem from "../Stations/StationList/StationListItem";
 import {getRecentStations} from "../store/RecentStations";
 import type {RecentStation} from "../types";
 
 import "../css/StationList.scss";
 
-const Recent = () => (
-  <>
-    <h3>Recent Stations</h3>
-    <ul className="station-list" elementtiming="station-list">
-      {getRecentStations().map(
-        ({pathname, title, lines}: RecentStation, index: number) => (
-          <li key={index}>
-            <StationListItem name={title} lines={lines} url={pathname} />
-          </li>
-        )
+const Recent = () => {
+  const stations = getRecentStations();
+  return (
+    <>
+      <h3>Recent Stations</h3>
+      {stations.length ? (
+        <ul className="station-list" elementtiming="station-list">
+          {getRecentStations().map(
+            ({pathname, title, lines}: RecentStation, index: number) => (
+              <li key={index}>
+                <StationListItem name={title} lines={lines} url={pathname} />
+              </li>
+            )
+          )}
+        </ul>
+      ) : (
+        <EmptyState />
       )}
-    </ul>
-  </>
-);
+
+      <Link to="/stations">View All Stations</Link>
+    </>
+  );
+};
 
 export default Recent;
