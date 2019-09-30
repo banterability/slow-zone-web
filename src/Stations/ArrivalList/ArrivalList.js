42c48fb018d76ae572689caa54cfe9006a28f028
Jeff Long <banterability@users.noreply.github.com>
2019-09-30 14:34:17 -0500

    Group arrivals by stop

--- src/Stations/ArrivalList/ArrivalList.js
+++ src/Stations/ArrivalList/ArrivalList.js
@@ -1,7 +1,9 @@
 // @flow
-import React from "react";
+import React, {Fragment} from "react";
+import {groupBy} from "lodash-es";
 
 import ArrivalListItem from "./ArrivalListItem";
+import ArrivalListSectionHeader from "./ArrivalListSectionHeader";
 import type {Arrival as ArrivalType} from "../../types";
 
 import "../../css/ArrivalList.scss";
@@ -12,19 +14,39 @@ type Props = {
   refresh: () => void
 };
 
-const ArrivalList = ({arrivals, loading, refresh}: Props) => (
-  <>
-    <div className="arrival-list__header">
-      <h3 className="arrival-list__title">Arrivals</h3>
-      {loading ? <p>loading...</p> : <button onClick={refresh}>Refresh</button>}
-    </div>
+const ArrivalList = ({arrivals, loading, refresh}: Props) => {
+  const arrivalsByStop = groupBy(arrivals, arrival => arrival.station.stop.id);
 
-    <ul className="arrival-list">
-      {arrivals.map((arrival, index) => (
-        <ArrivalListItem key={index} {...arrival} />
-      ))}
-    </ul>
-  </>
-);
+  return (
+    <>
+      <div className="arrival-list__header">
+        <h3 className="arrival-list__title">Arrivals</h3>
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <button className="arrival-list__refresh" onClick={refresh}>
+            Refresh
+          </button>
+        )}
+      </div>
+
+      <ul className="arrival-list">
+        {Object.values(arrivalsByStop).map(arrivalGroup => {
+          const {id, description} = arrivalGroup[0].station.stop;
+
+          return (
+            <Fragment key={`arrival-list:section:${id}`}>
+              <ArrivalListSectionHeader title={description} />
+
+              {arrivalGroup.map((arrival, index) => (
+                <ArrivalListItem key={index} {...arrival} />
+              ))}
+            </Fragment>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default ArrivalList;
---
+++ src/Stations/ArrivalList/ArrivalListSectionHeader.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from "react";
+
+import "../../css/ArrivalListSectionHeader.scss";
+
+type Props = {
+  title: string
+};
+
+const ArrivalListSectionHeader = ({title}: Props) => (
+  <li className="arrival-list__section-header">{title}</li>
+);
+
+export default ArrivalListSectionHeader;
