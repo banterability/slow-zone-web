c3dfafb284f6fb75ed0fefbc2f90250faad937f0
Jeff Long <banterability@users.noreply.github.com>
2019-11-01 16:20:17 -0500

    Refactor StaticMap component

--- src/Components/StaticMap.js
+++ src/Components/StaticMap.js
@@ -1,35 +1,31 @@
 import React from "react";
 
 import {googleStaticMapsKey} from "../config";
+import {buildQueryString} from "../lib/url";
 
 import "../css/StaticMap.css";
 
 const BASE_URL = "https://maps.googleapis.com/maps/api/staticmap";
+const LATITUDE_OFFSET = 0.0005;
 
-const getParams = props => {
-  const {latitude, longitude, width, height, ...passthrough} = props;
-  return Object.entries({
+const generateMapUrl = ({
+  height,
+  latitude,
+  longitude,
+  width,
+  ...passthrough
+}) => {
+  return `${BASE_URL}${buildQueryString({
     ...passthrough,
-    center: `${latitude + 0.0005},${longitude}`,
+    center: `${latitude + LATITUDE_OFFSET},${longitude}`,
     size: `${width}x${height}`
-  })
-    .reduce((memo, [key, value]) => {
-      memo.push(`${key}=${value}`);
-      return memo;
-    }, [])
-    .join("&");
-};
-
-const buildUrl = props => {
-  return `${BASE_URL}?${getParams(props)}`;
+  })}`;
 };
 
 const StaticMap = props => (
   <div
     className="static-map"
-    style={{
-      backgroundImage: `url(${buildUrl(props)})`
-    }}
+    style={{backgroundImage: `url(${generateMapUrl(props)})`}}
   />
 );
 
