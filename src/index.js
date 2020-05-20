3edf7acae4e1c4decfaad9d266a5724a526b468e
Jeff Long <banterability@users.noreply.github.com>
2020-05-20 10:23:19 -0500

    enable tracing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/browser";
+import {Integrations as ApmIntegrations} from "@sentry/apm";
 
 import App from "./App";
 
@@ -8,6 +9,8 @@ if (process.env.REACT_APP_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: process.env.REACT_APP_REVISION,
+    integrations: [new ApmIntegrations.Tracing()],
+    tracesSampleRate: 0.25,
   });
 }
 
