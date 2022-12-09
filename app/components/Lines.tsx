0a22c851511c4002e712fa5dc76fe4708e5fc6d6
Jeff Long <banterability@users.noreply.github.com>
2022-12-09 16:25:00 -0600

    line filtering

--- app/components/Lines.tsx
+++ app/components/Lines.tsx
@@ -1,8 +1,24 @@
-export function Lines({ lines }) {
+import classNames from "classnames";
+import { Line } from "~/types/station";
+
+type LinesProps = {
+  lines: [Line];
+  activeLines?: [Line];
+  onLineClick?: () => {};
+};
+
+export function Lines({ lines, activeLines = lines, onLineClick }: LinesProps) {
   return (
     <div className="lines">
-      {lines.map((line, index) => (
-        <div className={`line cta-${line}`} key={line} />
+      {lines.map((line: Line) => (
+        <div
+          className={classNames(`line cta-${line}`, {
+            "line--hidden": !activeLines.includes(line),
+            "line--clickable": Boolean(onLineClick),
+          })}
+          key={line}
+          {...(onLineClick && { onClick: () => onLineClick(line) })}
+        />
       ))}
     </div>
   );
--- app/routes/stations/index.tsx
+++ app/routes/stations/index.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node";
 import { Link, useCatch, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
 import { Lines } from "~/components/Lines";
 
 import { ORDERED_STATIONS } from "~/data/stations";
@@ -28,23 +29,74 @@ export function StationListItem({
     <li>
       <Link className="station-list__item" to={`/stations/${id}`}>
         <p className="station-list__item__name">{name}</p>
-
         <Lines lines={lines} />
       </Link>
     </li>
   );
 }
 
+function ListFilter({ lines, visibleLines, onLineClick }) {
+  return (
+    <Lines lines={lines} activeLines={visibleLines} onLineClick={onLineClick} />
+  );
+}
+
 export default function StationList() {
   const { stations } = useLoaderData();
+  const allLines = [
+    "blue",
+    "brown",
+    "green",
+    "orange",
+    "pink",
+    "purple",
+    "red",
+    "yellow",
+  ];
+  const [visibleLines, setVisibleLines] = useState(allLines);
+
+  const filteredStations = stations.filter((station: Station) => {
+    return station.lines
+      .map((line) => {
+        return visibleLines.includes(line);
+      })
+      .some((el) => Boolean(el));
+  });
+
+  const toggleLine = (line: string) => {
+    if (visibleLines.includes(line)) {
+      let idx = visibleLines.indexOf(line);
+      visibleLines.splice(idx, 1);
+      setVisibleLines([...visibleLines]);
+    } else {
+      visibleLines.push(line);
+      setVisibleLines([...visibleLines]);
+    }
+  };
 
   return (
-    <ul className="station-list">
-      {stations.map((station: Station) => {
-        const { id, name, lines } = station;
-        return <StationListItem key={id} id={id} name={name} lines={lines} />;
-      })}
-    </ul>
+    <>
+      <div className="line-filter-container">
+        <h4>Filter by line: </h4>
+        <ListFilter
+          lines={allLines}
+          visibleLines={visibleLines}
+          onLineClick={(line: string) => toggleLine(line)}
+        />
+        <button
+          disabled={visibleLines.length === 8}
+          onClick={() => setVisibleLines(allLines)}
+        >
+          Show All
+        </button>
+      </div>
+      <ul className="station-list">
+        {filteredStations.map((station: Station) => {
+          const { id, name, lines } = station;
+          return <StationListItem key={id} id={id} name={name} lines={lines} />;
+        })}
+      </ul>
+    </>
   );
 }
 
