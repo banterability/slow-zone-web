c4b62ed5f1dce1f8a0241466163a09bc6d7a5140
Jeff Long <banterability@users.noreply.github.com>
2023-06-11 06:55:25 -0500

    opt in to v2 header future flag

--- remix.config.js
+++ remix.config.js
@@ -19,6 +19,7 @@ module.exports = {
   serverDependenciesToBundle: ["slow-zone"],
   future: {
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
