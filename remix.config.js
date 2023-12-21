d5cc85352cb71c1d490bd202e086b09c9cfd594b
Jeff Long <banterability@users.noreply.github.com>
2023-12-21 00:26:07 -0600

    Remix 2.4.0

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatPath: true
+  },
   ignoredRouteFiles: ["**/.*"],
 };
