1b062da71a7e88b6cdc398eb71b11baa46c40480
Jeff Long <banterability@users.noreply.github.com>
2026-04-10 22:07:43 -0500

    remove tsconfigpaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   build: {
     sourcemap: "hidden",
   },
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     reactRouter(),
-    tsconfigPaths(),
     sentryVitePlugin({
       authToken: process.env.SENTRY_AUTH_TOKEN,
       org: "banterability",
