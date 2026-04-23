9233d5c7592adbcefa7e6322dc371f24b3206caf
Jeff Long <banterability@users.noreply.github.com>
2026-04-23 12:08:27 -0500

    eslint 10

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,97 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import importPlugin from "eslint-plugin-import";
+import eslintReact from "@eslint-react/eslint-plugin";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-config-prettier/flat";
+import importPlugin from "eslint-plugin-import-x";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+export default [
+  {
+    ignores: [
+      "api",
+      "build/**",
+      "node_modules/**",
+      "public/build/**",
+      ".react-router/**",
+    ],
+  },
 
-export default [{
-    ignores: ["api", "node_modules", "build", ".react-router"],
-}, ...compat.extends("prettier"), {
-    plugins: {
-        import: importPlugin,
+  js.configs.recommended,
+
+  eslintReact.configs["recommended-typescript"],
+
+  ...tsPlugin.configs["flat/recommended"],
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: { "import-x": importPlugin },
+    settings: {
+      "import-x/internal-regex": "^~/",
+      "import-x/resolver": {
+        node: { extensions: [".ts", ".tsx"] },
+        typescript: { alwaysTryTypes: true },
+      },
     },
     rules: {
-        "import/order": ["error", {
-            alphabetize: {
-                order: "asc",
-                caseInsensitive: true,
-            },
-
-            groups: ["builtin", "external", "internal", "parent", "sibling", "type"],
-            "newlines-between": "always",
-        }],
+      ...importPlugin.flatConfigs.recommended.rules,
+      ...importPlugin.flatConfigs.typescript.rules,
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
-}];
+  },
+
+  {
+    files: ["scripts/**/*.js"],
+    languageOptions: {
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        fetch: "readonly",
+        URL: "readonly",
+        URLSearchParams: "readonly",
+      },
+    },
+  },
+
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: { "import-x": importPlugin },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      "import-x/resolver": { typescript: {} },
+    },
+    rules: {
+      "import-x/order": [
+        "error",
+        {
+          alphabetize: { order: "asc", caseInsensitive: true },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "type",
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+
+  prettier,
+];
