215dc5a4afdffa4cf4b03609c1ea7da9c00817c7
Jeff Long <banterability@users.noreply.github.com>
2026-06-29 11:01:46 -0500

    React Router 8

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,13 +4,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  future: {
-    v8_middleware: true,
-    v8_splitRouteModules: true,
-    v8_viteEnvironmentApi: true,
-    v8_passThroughRequests: true,
-    v8_trailingSlashAwareDataRequests: true,
-  },
+  future: {},
   buildEnd: async ({
     viteConfig: viteConfig,
     reactRouterConfig: reactRouterConfig,
