d4533457e166b5916b99532bda620c219ca4c573
Jeff Long <banterability@users.noreply.github.com>
2026-07-01 09:40:15 -0500

    prettier

--- app/types/line.d.ts
+++ app/types/line.d.ts
@@ -1,9 +1,2 @@
 export type Line =
-  | "blue"
-  | "brown"
-  | "green"
-  | "orange"
-  | "pink"
-  | "purple"
-  | "red"
-  | "yellow";
+  "blue" | "brown" | "green" | "orange" | "pink" | "purple" | "red" | "yellow";
